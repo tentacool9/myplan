2d9eb8c032e935a41432b77e86169ea77e1cab7d
tentacool9 <leguyman@gmail.com>
2019-08-16 13:24:12 +0300

    startup script

--- app.bat
+++ app.bat
@@ -1,4 +1,7 @@
-echo start
+echo "starting backend"
 start cmd /k "cd myplan_backend & node server.js"
+echo "starting mongo"
+#edit the path to mongo
 start cmd /c ""C:\Program Files\MongoDB/Server\4.0\bin\mongo.exe""
+echo "starting frontend"
 cd myplan & npm run serve
